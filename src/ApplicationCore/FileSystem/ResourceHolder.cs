7300041e32dfd5b4ff6cb362a5f82a6b59c3f331
Andrei Epure <andrei.epure@sonarsource.com>
2023-08-19 21:09:38 +0200

    add ResourceHolder

---
+++ src/ApplicationCore/FileSystem/ResourceHolder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.eShopWeb.ApplicationCore.FileSystem;
+public class ResourceHolder
+{
+    private FileStream fs; // Noncompliant: dispose or close are never called
+
+    public ResourceHolder(string path)
+    {
+        this.fs = new FileStream(path, FileMode.Open);
+    }
+
+    public void WriteToFile(string path, string text)
+    {
+        var fs = new FileStream(path, FileMode.Open); // Noncompliant: not disposed, returned or initialized with another disposable object
+        var bytes = Encoding.UTF8.GetBytes(text);
+        fs.Write(bytes, 0, bytes.Length);
+    }
+}

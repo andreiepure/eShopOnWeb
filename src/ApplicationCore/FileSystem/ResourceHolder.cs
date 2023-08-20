414216d2f69480fa626ac6697671ad507040a87e
Andrei Epure <andrei.epure@sonarsource.com>
2023-08-20 16:52:24 +0200

    add coverage, fix bugs

--- src/ApplicationCore/FileSystem/ResourceHolder.cs
+++ src/ApplicationCore/FileSystem/ResourceHolder.cs
@@ -8,17 +8,11 @@ using System.Threading.Tasks;
 namespace Microsoft.eShopWeb.ApplicationCore.FileSystem;
 public class ResourceHolder
 {
-    private FileStream fs; // Noncompliant: dispose or close are never called
-
-    public ResourceHolder(string path)
-    {
-        this.fs = new FileStream(path, FileMode.Open);
-    }
-
     public void WriteToFile(string path, string text)
     {
-        var fs = new FileStream(path, FileMode.Open); // Noncompliant: not disposed, returned or initialized with another disposable object
+        var fs = new FileStream(path, FileMode.OpenOrCreate);
         var bytes = Encoding.UTF8.GetBytes(text);
         fs.Write(bytes, 0, bytes.Length);
+        fs.Close();
     }
 }
---
+++ tests/UnitTests/ApplicationCore/FileSystem/ResourceHolderTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.eShopWeb.ApplicationCore.FileSystem;
+using Xunit;
+
+namespace Microsoft.eShopWeb.UnitTests.ApplicationCore.FileSystem;
+public class ResourceHolderTests
+{
+    [Fact]
+    public void WritesFileToDisk()
+    {
+        var underTest = new ResourceHolder();
+        underTest.WriteToFile("test", "foo");
+        
+        Assert.True(File.Exists("test"));
+    }
+}

8cc1f35631844a49701c6083d845e532fa6c4633
Tim <caycdotnet.tim.the.junior@gmail.com>
2023-08-23 19:54:38 +0200

    add cat search

---
+++ src/ApplicationCore/Services/CatPhotoService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Microsoft.eShopWeb.ApplicationCore.Services;
+
+public class CatPhotoService
+{
+    public CatPhotoService()
+    {
+
+    }
+
+    public bool HasCat(string catName)
+    {
+        var process = System.Diagnostics.Process.Start("search.cmd", catName);
+        process.WaitForExit();
+        if (IsPalindrome(catName))
+        {
+            return false;
+        } else
+        {
+            return process.ExitCode == 0;
+        }
+    }
+
+    public bool IsPalindrome(string dogName)
+    {
+        string first = dogName.Substring(0, dogName.Length / 2);
+        char[] arr = dogName.ToCharArray();
+
+        Array.Reverse(arr);
+
+        string temp = new string(arr);
+        string second = temp.Substring(0, temp.Length / 2);
+
+        return first.Equals(second);
+    }
+}
---
+++ src/Web/ViewModels/CatViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Microsoft.eShopWeb.Web.ViewModels;
+
+public class CatViewModel
+{
+    public CatViewModel(string catName)
+    {
+        CatName = catName;
+    }
+    public string CatName { get; set; }
+    public string? CatPhotoPath { get; set; }
+}
